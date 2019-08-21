5e20b43cebae403cea1e24454e644f2b5619bfeb
gothami <gothamisa92@gmsil.com>
2019-08-22 04:50:48 +0930

    Add project files.

---
+++ Test/Customer.cs
@@ -0,0 +1,12 @@
+﻿namespace Test
+{
+    public class Comment
+    {
+        public int postId { get; set; }
+        public int id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }       
+        public string body { get; set; }       
+    }
+}
+
---
+++ Test/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Web.Script.Serialization;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serializer = new JavaScriptSerializer();
+
+            string filePath = "E:\\Visual Studio Files\\JSONText.txt";
+            string result = File.ReadAllText(filePath);
+            List<Comment> comments = (List<Comment>)serializer.Deserialize(result, typeof(List<Comment>));
+
+            foreach(Comment comment in comments)
+            {
+                Console.WriteLine(comment.name + "," + comment.id + "," + comment.email + "," + comment.body + "," + comment.postId);                
+            }            
+
+            Console.ReadLine();
+        }
+    }
+}
